3b02d1d929923c7fb8441145cd62f7221eee3067
Tin Tran <tintranfs@gmail.com>
2024-02-19 18:52:10 +0700

    Update component 404

--- src/components/common/errors/404/index.tsx
+++ src/components/common/errors/404/index.tsx
@@ -7,13 +7,13 @@ const NotFound: FC = () => {
   return (
     <div className="grid place-items-center">
       <div className="text-center">
-        <h1 className="text-bolder mt-20 mb-5 text-2xl font-bold leading-normal sm:text-3xl">404</h1>
-        <div className="mb-11">
-          <h1 className="text-h3">Page not found</h1>
+        <h1 className="text-bolder mt-20 mb-2 text-2xl font-bold leading-normal sm:text-3xl font-inter">404</h1>
+        <div className="mb-8">
+          <h1 className="text-h3 font-inter">Page not found</h1>
         </div>
         <Link
           href={'/'}
-          className="inline-flex items-center underline hover:no-underline focus:outline-none sm:text-base"
+          className="font-inter inline-flex items-center underline hover:no-underline focus:outline-none sm:text-base"
         >
           Back to Home
         </Link>
